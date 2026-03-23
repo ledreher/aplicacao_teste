8cac35c808d9b9d4717c948ab2df343ed5c5e5b7
ledreher <leticiaeduardadreherdreher@gmail.com>
2026-03-23 19:58:58 -0300

    Primeiro codigo (lero lero)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hellojjjjjjjjjjjjjjjjjjj World!'),
-        ),
-      ),
-    );
-  }
+        body: Text("lero lero")
+      )
+    )
+  );
 }
